6e99a467f2aba033ee3da0790b59e9cf4fc5d2de
unknown <jenatcanil14@gmail.com>
2024-07-24 15:17:35 +0530

    Updated File

--- src/components/CartData.tsx
+++ src/components/CartData.tsx
@@ -1,19 +1,32 @@
-import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/Button"
-import {
-    Card,
-    CardContent,
-    CardFooter,
-  } from "@/components/ui/Card";
+import React from "react";
+import { Button } from "@/components/ui/Button";
 
-export default function CartData(){
-    return(
-        <>
-            <Card className="flex justify-between">
-                <CardContent>
-                    
-                </CardContent>
-            </Card>
-        </>
-    )
-}
+type Product = {
+    id: number,
+    title: string,
+    price: number,
+    image: string
+};
+
+type CartDataProps = {
+    cartProducts: Product[];
+};
+
+const CartData: React.FC<CartDataProps> = ({ cartProducts }) => {
+    const totalItems = cartProducts.reduce((total, product) => total + 1, 0);
+    const totalPrice = cartProducts.reduce((total, product) => total + product.price, 0);
+
+    return (
+        <div className="text-center flex gap-3 justify-evenly my-4">
+            <div className="bg-gray-200 w-[400px] p-4 rounded-lg">
+                <p>Total Items: {totalItems}</p>
+            </div>
+            <div className="bg-gray-200 w-[400px] p-4 rounded-lg">
+            <p>Total Price: ${totalPrice.toFixed(2)}</p>
+            </div>
+            <Button className="h-auto w-[400px]" disabled={totalItems === 0} variant="destructive">Proceed to by {totalItems} item(s) </Button>
+        </div>
+    );
+};
+
+export default CartData;
