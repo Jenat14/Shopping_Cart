f362110338e3f525b965ae37bf4c2bfca97f9c1c
unknown <jenatcanil14@gmail.com>
2024-07-24 13:16:45 +0530

    Added file

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -1,8 +1,11 @@
-import NavBar from "../components/NavBar";
+import NavBar from "@/components/NavBar"
+import CartData from "@/components/CartData"
+import CartProduct from "@/components/CartProduct"
 export default function Cart(){
     return(
         <>
             <NavBar/>
+            <CartData/>
         </>
     )
 }
