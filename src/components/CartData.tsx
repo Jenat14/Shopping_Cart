1e1a830e9556aaaebd1b132b8de71eb2941bdbaa
unknown <jenatcanil14@gmail.com>
2024-07-24 13:15:47 +0530

    Modified file

---
+++ src/components/CartData.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/Button"
+import {
+    Card,
+    CardContent,
+    CardFooter,
+  } from "@/components/ui/Card";
+
+export default function CartData(){
+    return(
+        <>
+            <Card className="flex justify-between">
+                <CardContent>
+                    
+                </CardContent>
+            </Card>
+        </>
+    )
+}
