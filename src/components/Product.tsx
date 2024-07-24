45d827840a4117b388ef6f8e9a42edaa66cd1be1
unknown <jenatcanil14@gmail.com>
2024-07-24 13:17:05 +0530

    Modified file

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,64 +8,70 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/Card";
+} from "@/components/ui/Card";
 hourglass.register()
 
 type Product = {
-  id: number,
-  title: string,
-  price: number,
-  description: string,
-  category: string,
-  image: string
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string
 };
 
+const AddToCart = (product: Product) => {
+    const CartItem = localStorage.getItem("cart");
+    const cart: Product[] = CartItem ? JSON.parse(CartItem) : [];
+
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart))
+    console.log(cart);
+}
+
 export default function Product() {
-  const [result, setResult] = useState<Product[] | null>(null);
+    const [result, setResult] = useState<Product[] | null>(null);
 
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(json => setResult(json))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => setResult(json))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
-  console.log({ result });
+    console.log({ result });
 
-  return (
-    <div className="container p-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {result ? (
-          result.map(product => (
-            <>
-            <Card className="text-center flex flex-col justify-between">
-                <CardHeader>
-                    <CardTitle>{product.title}</CardTitle>
-                    <CardDescription className="h-[20px] truncate">{product.description}</CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center justify-center">
-                <img className="h-[150px]"src={product.image} alt={product.title} />
-                <p>Price: ${product.price}</p>
-                <p>Category: {product.category}</p>
-                </CardContent>
-                <CardFooter className="flex items-center gap-3 justify-center">
-                <Button variant="destructive">Add to cart <img className="ml-2 h-[70%]" src="src/assets/cart.png"></img></Button>
-                <Button variant="default">See more</Button>
-                </CardFooter>
-            </Card>
-            </>
-          ))
-        ) : (
-                <div className="h-[100vh] w-[100vw] flex justify-center items-center">
-                    <l-hourglass
-                    size="40"
-                    bg-opacity="0.1"
-                    speed="1.75"
-                    color="black"
-                    ></l-hourglass>
-                </div>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container p-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {result ? (
+                    result.map(product => (
+                        <Card key={product.id} className="text-center flex flex-col justify-between">
+                            <CardHeader>
+                                <CardTitle>{product.title}</CardTitle>
+                                <CardDescription className="h-[20px] truncate">{product.description}</CardDescription>
+                            </CardHeader>
+                            <CardContent className="flex flex-col items-center justify-center">
+                                <img className="h-[150px]" src={product.image} alt={product.title} />
+                                <p>Price: ${product.price}</p>
+                                <p>Category: {product.category}</p>
+                            </CardContent>
+                            <CardFooter className="flex items-center gap-3 justify-center">
+                                <Button variant="destructive" onClick={() => AddToCart(product)}>Add to cart <img className="ml-2 h-[70%]" src="src/assets/cart.png" alt="Cart Icon" /></Button>
+                                <Button variant="default">See more</Button>
+                            </CardFooter>
+                        </Card>
+                    ))
+                ) : (
+                    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+                        <l-hourglass
+                            size="40"
+                            speed="1.75"
+                            color="black"
+                        ></l-hourglass>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
