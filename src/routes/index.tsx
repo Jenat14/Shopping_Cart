9d76821235fd3ba745f8f90696acaea35648c371
unknown <jenatcanil14@gmail.com>
2024-07-24 13:17:36 +0530

    Modified file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
-import Home from "../Pages/Home";
+import Home from "@/Pages/Home";
+import Cart from "@/Pages/Cart";
 export default function Navigator(){
     return(
         <>
             <Routes>
                 <Route path={"/"} element={<Home />}></Route>
+                <Route path={"/Cart"} element={<Cart/>}></Route>
             </Routes>
         </>
     )
