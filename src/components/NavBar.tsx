947a5f8cbf802749de08b96212a165cf039c85bf
unknown <jenatcanil14@gmail.com>
2024-07-24 10:17:22 +0530

    Style changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 function NavBar(){
     return(
     <>
-        <div className="container content-center p-2 bg-[#201E43] flex items-center justify-between">
+        <div className="container content-center p-2 bg-[#201E43] flex items-center">
                 <div className="self-center ml-3.5">
                     <a href="#"><img className="w-[50px]" src="src\assets\logo.svg"></img></a>
                 </div>
@@ -9,10 +9,10 @@ function NavBar(){
                     <input type="text"
                     id="items"
                     className="px-3 py-2 border border-gray-300 w-[500px] h-[40px]"
-                    placeholder="Search For Products"/>
+                    placeholder="Search By Category"/>
                     <button className="bg-[#508C9B] h-[40px] w-[50px]"><img className="h-[20px] ml-3 w-[auto]"src="src\assets\Search.png"/></button>
                 </div>
-                <div className="content-center flex items-center text-white">
+                <div className="content-center mr-3 flex items-center text-white">
                     <a href="#" className="m-4 hover:text-zinc-400">Home</a>
                     <a href="#" className="m-4 hover:text-zinc-400">About</a>
                     <a href="#" className="m-4 hover:text-zinc-400">Login</a>
