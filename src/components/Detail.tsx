462fe62be5a77b42e34f1f5c1ab1addf701b600b
unknown <jenatcanil14@gmail.com>
2024-07-24 16:08:31 +0530

    Updated file

---
+++ src/components/Detail.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type Product = {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string
+};
+
+type DetailProps = {
+    product: Product;
+};
+
+const Detail: React.FC<DetailProps> = ({ product }) => {
+    return (
+        <div className="container p-2">
+            <div className="flex gap-3">
+            <img src={product.image} alt={product.title} className="w-[300px] h-[400px]" />
+                <div>
+                    <h2 className="font-bold text-2xl mb-4">{product.title}</h2>
+                    <p>Category: {product.category}</p>
+                    <p className='text-justify my-4 max-h-[200px] overflow-y-auto'>{product.description}</p>
+                    <p className='text-lg font-bold'>Price: ${product.price}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Detail;
